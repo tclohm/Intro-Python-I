0e53c668a831456369e4835d6de68da14c985566
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-04-06 19:41:34 -0700

    finished args

--- src/11_args.py
+++ src/11_args.py
@@ -15,8 +15,8 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-def f2(*args):
-	return sum([el for el in args])
+def f2(*args): 
+	return sum(args[0]) if type(args[0]) == list else sum([el for el in args])
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -34,6 +34,11 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+# def f3(*args):
+# 	return args[0] + 1 if len(args) == 1 else sum([element for element in args])
+
+def f3(v1, v2=1):
+	return v1 + v2
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -48,6 +53,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(*args ,**kwargs):
+	dictionary = kwargs if kwargs else args[0]
+	for key, value in dictionary.items():
+		print(f"key: {key}, value: {value}")
 
 # Should print
 # key: a, value: 12
